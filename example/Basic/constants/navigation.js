607ea040ff65dc95e376b0d91c6d2459eeb2b4c6
Kureev Alexey <kureev-mail@ya.ru>
2016-02-01 17:55:48 +0100

    Example using navigator-redux package

--- example/Basic/constants/navigation.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PUSH: 'PUSH',
-  POP: 'POP',
-  REPLACE: 'REPLACE',
-};
--- example/Basic/index.ios.js
+++ example/Basic/index.ios.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 import React, {
   AppRegistry,
   Component,
@@ -11,17 +7,12 @@ import React, {
   View,
 } from 'react-native';
 
-import { Provider, connect } from 'react-redux';
-import reducer from './reducers/navigate';
-import makeNavState from './makeNavState';
-import actions from './actions/navigation';
-import NavigatorRedux from './components/NavigatorRedux';
+import {Provider, connect} from 'react-redux';
+import {NavigatorRedux, makeNavState, navigationReducer} from 'navigator-redux';
 import Content from './Content';
-import { createStore, bindActionCreators } from 'redux';
+import {createStore, bindActionCreators} from 'redux';
 
-const store = createStore(reducer, makeNavState([
-  <Content />,
-]));
+const store = createStore(navigationReducer, makeNavState([<Content/>]));
 
 AppRegistry.registerComponent('Basic', () => () => (
   <Provider store={store}>
