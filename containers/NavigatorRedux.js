897548c56d89fa4cd1de71c4e27d1e5266871a2c
Kureev Alexey <kureev-mail@ya.ru>
2016-02-07 22:24:58 +0100

    PropTypes and other fancy fixes

--- containers/NavigatorRedux.js
+++ containers/NavigatorRedux.js
@@ -1,10 +1,11 @@
 const React = require('react-native');
+const Immutable = require('immutable');
 const {bindActionCreators} = require('redux');
 const {connect} = require('react-redux');
 const actions = require('../actions');
 const invariant = require('invariant');
 
-const {View, StyleSheet} = React;
+const {View, StyleSheet, PropTypes} = React;
 const styles = StyleSheet.create({container: {flex: 1}});
 
 /**
@@ -12,10 +13,12 @@ const styles = StyleSheet.create({container: {flex: 1}});
  * @param  {Number} options.index Navigation stack's current index
  * @param  {Immutable.Stack} options.stack Navigation stack
  * @param  {Object} options.actions Navigator's actions
+ * @param  {Function} options.renderer Container renderer function
  * @param  {Number|Object} options.style Custom navigator style
  * @return {React.Element}
  */
-function renderNavigator({index, stack, actions, style}) {
+function renderNavigator(props) {
+  const {index, stack, actions, renderer, style} = props;
   const route = stack.get(index);
 
   invariant(route,
@@ -23,12 +26,26 @@ function renderNavigator({index, stack, actions, style}) {
     'use `makeNavState`,seems you missed to fulfill it with data!'
   );
 
-  return (
-    <View style={[styles.container, style]}>
-      {React.cloneElement(route, {nav: {route, index, stack, actions}})}
-    </View>
+  return renderer(
+    React.cloneElement(route, {nav: {route, index, stack, actions}})
   );
 }
+
+renderNavigator.propTypes = {
+  index: PropTypes.number.isRequired,
+  stack: PropTypes.instanceOf(Immutable.Stack).isRequired,
+  actions: PropTypes.object.isRequired,
+  renderer: PropTypes.func,
+  style: PropTypes.oneOfType([PropTypes.number, PropTypes.object]),
+};
+
+renderNavigator.defaultProps = {
+  renderer: (route) => (
+    <View style={styles.container}>
+      {route}
+    </View>
+  ),
+};
 
 module.exports = connect(
   (state) => ({
